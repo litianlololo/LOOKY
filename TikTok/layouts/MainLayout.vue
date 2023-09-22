86deeed28d38370acc17b336daeb3b45033bfb0e
litianlololo <102678203+litianlololo@users.noreply.github.com>
2023-09-22 20:57:20 +0800

    1.00.39

---
+++ TikTok/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <TopNav />
+    <div 
+        :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''" 
+        class="flex justify-between mx-auto w-full "
+    >
+        <div>
+            <SideNavMain />
+        </div>
+        <slot/>
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
