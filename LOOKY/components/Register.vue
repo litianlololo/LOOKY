4df980a35d50bd72f353b3cf73b176cfa5aa360e
litianlololo <ajxflth@163.com>
2023-10-04 23:19:28 +0800

    实现注册

--- LOOKY/components/Register.vue
+++ LOOKY/components/Register.vue
@@ -13,7 +13,7 @@
 
     <div class="px-6 pb-2">
         <TextInput 
-            placeholder="Email address"
+            placeholder="Email address" 
             v-model:input="email"
             inputType="email"
             error=""
@@ -60,4 +60,41 @@ let password = ref(null)
 let confirmPassword = ref(null)
 let errors = ref(null)
 
+const register = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // 设置请求头为 JSON
+      },
+      body: JSON.stringify({ // 将请求数据序列化为 JSON 字符串
+        name: name.value, 
+        email: email.value,
+        password: password.value,
+      }),
+    })
+    
+    const data = await response.json();
+    console.log(data); // 打印响应数据
+    
+    if(data.statusCode == 200){
+
+        $userStore.id = data.user.id;
+        $userStore.name = data.user.name;
+        $userStore.bio = data.user.bio;
+        $userStore.RegistrationDate = data.user.RegistrationDate;
+        $userStore.email = data.user.email;
+        $generalStore.isLoginOpen = false;
+        //userStore.image = data.image;
+        console.log($userStore.email);
+    }
+    else{
+        message.error(data.message)
+    }
+  } catch (error) {
+    console.log(error.json);
+    // 处理登录错误
+    //errors.value = "登录失败，请检查您的用户名和密码。";
+  }
+};
 </script>
